6c5a19d008bbbb200dcf0a659535f256fb183078
nvdong04 <vandong1x99@gmail.com>
2023-10-11 16:55:20 +0700

    add product detail

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,9 @@
+function ProductDetail() {
+    return ( 
+        <>
+            https://codepen.io/mithicher/pen/ExyBbjJ
+        </>
+     );
+}
+
+export default ProductDetail;
---
+++ src/components/ProductItem.js
@@ -0,0 +1,211 @@
+function ProductItem() {
+    return ( 
+        <>
+            <div class="w-[1190px] mx-auto my-auto">
+  <div class="flex flex-col w-full h-auto mt-[3%]">
+    <div class="flex flex-wrap">
+    <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
+        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
+          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
+            <div>
+              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
+                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
+                  <i class="fa-solid fa-heart" style="color: #cc3535;"></i>
+                </div>
+              </span>
+            </div>
+          </div>
+
+          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
+
+            <div class="relative">
+              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
+                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
+                  <img src="https://bizweb.dktcdn.net/100/443/045/products/xe-dap-dien-jvc-eco-g9s-xi-mang-9c68291e-c586-4a10-8f84-443b142a742a.png?v=1691926303330" alt=""/>
+                </div>
+              </div>
+            </div>
+            <div class="relative p-2 box-border overflow-hidden grow">
+              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
+                iPhone 14 Pro Max
+              </span>
+              <div>
+                <div class="block w-full text-sm font-bold text-amber-700">
+                  $ 1900
+                </div>
+              </div>
+              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
+                iBox
+              </div>
+              <div class="min-h-[14px] text-xs">
+                <div class="align-items-center my-1 mx-0">
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <span class="align-midle ml-1 text-gray-600">
+                    (1400)
+                  </span>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
+        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
+          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
+            <div>
+              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
+                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
+                  <i class="fa-solid fa-heart" style="color: #949494;"></i>
+                </div>
+              </span>
+            </div>
+          </div>
+          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
+            <div class="relative">
+              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
+                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
+                  <img src="https://bizweb.dktcdn.net/100/443/045/products/xe-dap-dien-jvc-eco-g9s-xi-mang-9c68291e-c586-4a10-8f84-443b142a742a.png?v=1691926303330" alt=""/>
+                </div>
+              </div>
+            </div>
+            <div class="relative p-2 box-border overflow-hidden grow">
+              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
+                iPhone 14 Pro Max
+              </span>
+              <div>
+                <div class="block w-full text-sm font-bold text-amber-700">
+                  $ 1900
+                </div>
+              </div>
+              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
+                iBox
+              </div>
+              <div class="min-h-[14px] text-xs">
+                <div class="align-items-center my-1 mx-0">
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star-half" style="color: #7a7600;"></i>
+                  <span class="align-midle ml-1 text-gray-600">
+                    (1400)
+                  </span>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
+        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
+          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
+            <div>
+              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
+                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
+                  <i class="fa-solid fa-heart" style="color: #949494;"></i>
+                </div>
+              </span>
+            </div>
+          </div>
+          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
+            <div class="relative">
+              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
+                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
+                  <img src="https://fakeimg.pl/400x400/a5edc0/616161?text=product" alt=""/>
+                </div>
+              </div>
+            </div>
+            <div class="relative p-2 box-border overflow-hidden grow">
+              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
+                iPhone 14 Pro Max
+              </span>
+              <div>
+                <div class="block w-full text-sm font-bold text-amber-700">
+                  $ 1900
+                </div>
+              </div>
+              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
+                iBox
+              </div>
+              <div class="min-h-[14px] text-xs">
+                <div class="align-items-center my-1 mx-0">
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-regular fa-star" style="color: #7a7600;"></i>
+                  <span class="align-midle ml-1 text-gray-600">
+                    (1400)
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
+        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
+          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
+            <div>
+              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
+                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
+                  <i class="fa-solid fa-heart" style="color: #949494;"></i>
+                </div>
+              </span>
+            </div>
+          </div>
+          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
+            <div class="relative">
+              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
+                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
+                  <img src="https://fakeimg.pl/400x400/a5edc0/616161?text=product" alt=""/>
+                </div>
+              </div>
+            </div>
+            <div class="relative p-2 box-border overflow-hidden grow">
+              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
+                iPhone 14 Pro Max
+              </span>
+              <div>
+                <div class="block w-full text-sm font-bold text-amber-700">
+                  $ 1900
+                </div>
+              </div>
+              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
+                iBox
+              </div>
+              <div class="min-h-[14px] text-xs">
+                <div class="align-items-center my-1 mx-0">
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
+                  <span class="align-midle ml-1 text-gray-600">
+                    (1400)
+                  </span>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+  </div>
+</div>
+        </>
+     );
+}
+
+export default ProductItem;
